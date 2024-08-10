8c591031e8a6d30a358336daf1d7d314ffd14607
DR7-ZOMBIE <dcr7juilland@gmail.com>
2024-08-10 17:00:05 -0500

    Update portafolio 2024

--- src/Components/HerramientasCiberseguridad.jsx
+++ src/Components/HerramientasCiberseguridad.jsx
@@ -1,40 +1,79 @@
 import React from 'react';
-import CardCarousel from './CardCarrousel';// Asegúrate de ajustar la ruta de importación según sea necesario
+import Slider from 'react-slick';
+import CardCarousel from './CardCarrousel'; // Asegúrate de ajustar la ruta de importación según sea necesario
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const HerramientasCiberseguridad = () => {
-    const herramientas = [
+const ProyectosGitHub = () => {
+    const proyectos = [
         {
             id: 1,
-            titulo: "Wireshark",
-            descripcion: "Wireshark es un analizador de protocolos de red que permite capturar y examinar detalladamente el tráfico en una red en tiempo real. Es ampliamente utilizado para la resolución de problemas de red, análisis y desarrollo de software, y educación en ciberseguridad.",
-            imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.9dj9ZPnFfXnrFUp2cFfKfQHaEK%26pid%3DApi&f=1&ipt=a4ccbb9e87d84473a32dece005b5f106692da14399147b075b6fc9bdfe8e13fc&ipo=images",
-            urlDocumentacion:"https://www.wireshark.org/docs/"
+            titulo: "PhishInOut AI",
+            descripcion: "Un modelo de inteligencia artificial para la detección de correos electrónicos de phishing. Este proyecto incluye el desarrollo de un sistema basado en redes neuronales que distingue entre correos electrónicos maliciosos y benignos.",
+            imagen: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+            urlRepositorio: "https://github.com/tu-usuario/phishinout-ai"
         },
         {
             id: 2,
-            titulo: "Metasploitable",
-            descripcion: " Metasploit es un marco (framework) de prueba de penetración muy utilizado que facilita la búsqueda de vulnerabilidades en sistemas y redes. Permite a los profesionales de la seguridad desarrollar y ejecutar exploits contra objetivos remotos para probar su seguridad.",
-            imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.todavianose.com%2Fwp-content%2Fuploads%2F2016%2F11%2Fmetasploit-and-metasploitable.1280x600.jpg&f=1&nofb=1&ipt=9bb9d39fee753f43672b9e7d30bf07738f107eb8052c43893f87d8bfa2ee5f01&ipo=images",
-            urlDocumentacion:"https://docs.rapid7.com/metasploit/metasploitable-2/"
+            titulo: "Automated ISM3 Evaluations",
+            descripcion: "Este proyecto automatiza la evaluación de controles ISM3 mediante scripts en Python, permitiendo una mayor eficiencia y precisión en la auditoría de sistemas.",
+            imagen: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+            urlRepositorio: "https://github.com/tu-usuario/automated-ism3-evaluations"
         },
         {
             id: 3,
-            titulo: "Nessus",
-            descripcion: "Nessus es una de las herramientas de escaneo de vulnerabilidades más conocidas y utilizadas. Proporciona un análisis rápido y preciso de las vulnerabilidades de seguridad en sistemas y redes para ayudar a identificar y corregir fallos antes de que puedan ser explotados.",
-            imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F4.bp.blogspot.com%2F-LFIkoYYo32Q%2FWVSqL60OuxI%2FAAAAAAAAKAM%2F8zHaJNm77NAPck0yTrgJCvFaTbH_2uc_QCLcBGAs%2Fs1600%2FNessus-Vulnerability-Scanner.jpg&f=1&nofb=1&ipt=aed4142cbd2bc5aaa1b4d6339387da0159d9951db9bf4138aa1ce6ff856a82ec&ipo=images",
-            urlDocumentacion:"https://docs.tenable.com/Nessus.htm"
+            titulo: "Cloudflare WAF Configuration Guide",
+            descripcion: "Una guía exhaustiva para configurar correctamente el WAF de Cloudflare, destinada a mejorar la seguridad y rendimiento de aplicaciones web mediante la implementación de las mejores prácticas.",
+            imagen: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+            urlRepositorio: "https://github.com/tu-usuario/cloudflare-waf-configuration-guide"
         },
         
-        // Agrega más herramientas aquí
+        // Agrega más proyectos aquí
     ];
 
+    // Configuración del carrusel
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1
+                }
+            }
+        ]
+    };
+
     return (
-        <div className="flex flex-wrap justify-center gap-10 p-20">
-            {herramientas.map((herramienta) => (
-                <CardCarousel key={herramienta.id} titulo={herramienta.titulo} descripcion={herramienta.descripcion} imagen={herramienta.imagen} urlDocumentacion={herramienta.urlDocumentacion} />
-            ))}
+        <div className="p-20">
+            <Slider {...settings}>
+                {proyectos.map((proyecto) => (
+                    <CardCarousel 
+                        key={proyecto.id} 
+                        titulo={proyecto.titulo} 
+                        descripcion={proyecto.descripcion} 
+                        imagen={proyecto.imagen} 
+                        urlDocumentacion={proyecto.urlRepositorio} 
+                    />
+                ))}
+            </Slider>
         </div>
     );
 };
 
-export default HerramientasCiberseguridad;
+export default ProyectosGitHub;
--- src/Components/MiCV.jsx
+++ src/Components/MiCV.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import '../CSS/MiCV.css'
+import '../CSS/MiCV.css';
 
 // Asegúrate de que la ruta al PDF es correcta
 import cvPDF from '../assets/pdf/Spanish_Version.pdf';
@@ -11,25 +11,36 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 const MiCV = () => {
     const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
 
-    // Esta función se llama cada vez que se carga con éxito el documento PDF
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
 
-    // Renderiza el componente
+    const handleClick = () => {
+        setPageNumber(prevPageNumber => 
+            prevPageNumber < numPages ? prevPageNumber + 1 : 1
+        );
+    };
+
     return (
-        <div className="pdf-container flex justify-center items-center mt-3">
-        <Document
-            file={cvPDF}
-            onLoadSuccess={onDocumentLoadSuccess}
-            className="mt-7 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl rounded-lg overflow-hidden max-w-xl mx-auto border-2 border-gray-300"
-        >
-            {numPages ? (
-                <Page pageNumber={1} width={window.innerWidth > 768 ? 600 : window.innerWidth - 20}/>
-            ) : null}
-        </Document>
-    </div>
+        <div className="pdf-container flex justify-center items-center mt-5 ml-28"> {/* Ajuste de margen superior */}
+            <Document
+                file={cvPDF}
+                onLoadSuccess={onDocumentLoadSuccess}
+                className="transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl rounded-lg overflow-hidden max-w-xl mx-auto border-2 border-gray-300 cursor-pointer" 
+                onClick={handleClick} // Maneja el clic para cambiar de página
+            >
+                <Page 
+                    pageNumber={pageNumber} 
+                    width={window.innerWidth > 500 ? 600 : window.innerWidth - 20}
+                    scale={0.8} // Reduce la escala para disminuir la altura del PDF
+                />
+            </Document>
+            <div className="flex justify-center mt-4"> {/* Centrando el texto */}
+                <p>Página {pageNumber} de {numPages}</p>
+            </div>
+        </div>
     );
 };
 
